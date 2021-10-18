e7b70f9c7a6ea12477833c1b754219eb4532494a
Bibote <millan.saez@gmail.com>
2021-10-18 19:47:05 +0200

    aaaaa

--- src/main/java/business/logic/BLFacadeImplementation.java
+++ src/main/java/business/logic/BLFacadeImplementation.java
@@ -50,7 +50,7 @@ import exceptions.WrongEmailPattern;
 import exceptions.WrongParameters;
 
 /**
- * It implements the business logic as a web service.
+ * It implements the business logic as a web service.aaaaa
  */
 @WebService(endpointInterface = "businessLogic.BLFacade")
 public class BLFacadeImplementation  implements BLFacade {
